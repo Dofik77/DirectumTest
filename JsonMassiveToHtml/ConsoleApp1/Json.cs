582b910ee58fa1a11610227ddcaa688c298ae585
Dofik77 <55503282+Dofik77@users.noreply.github.com>
2021-02-19 02:24:25 +0300

    Directum Json to HTML Complete

---
+++ JsonMassiveToHtml/ConsoleApp1/Json.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ConsoleApp1
+{
+
+    public class Rootobject
+    {
+        public Form form { get; set; }
+    }
+
+    public class Form
+    {
+        public string name { get; set; }
+        public string postmessage { get; set; }
+        public Item[] items { get; set; }
+    }
+
+    public class Item
+    {
+        public string type { get; set; }
+        public Attributes attributes { get; set; }
+    }
+
+    public class Attributes
+    {
+        public string message { get; set; }
+        public string name { get; set; }
+        public string placeholder { get; set; }
+        public bool required { get; set; }
+        public string value { get; set; }
+        public string label { get; set; }
+        [JsonPropertyName("class")]
+        public string ClassName { get; set; }
+        public Validationrule[] validationRules { get; set; }
+        public bool disabled { get; set; }
+        public bool selected { get; set; }
+        [JsonPropertyName("checked")]
+        public bool check { get; set; }
+    }
+
+    public class Validationrule
+    {
+        public string type { get; set; }
+    }
+
+}
---
+++ JsonMassiveToHtml/ConsoleApp1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rootobject test = JsonConvert.DeserializeObject<Rootobject>(File.ReadAllText(@"D:\C#\JsonMassiveToHtml\ConsoleApp1\bin\Debug\netcoreapp3.1\json-Massive.json"));
+            
+            StringBuilder result = new StringBuilder();
+            result.Append($"<Html><head></head><body><form name=\"{test.form.items[0].attributes.name}\">");
+              
+            foreach(var item in test.form.items){
+                StringBuilder input = new StringBuilder();
+                if(!string.IsNullOrEmpty(item.attributes.label))
+                {
+                    input.Append($"<label for=\"{item.attributes.name}\"> {item.attributes.label}</label>"); 
+                }
+                input.Append("<input");
+                if(item.attributes.validationRules != null && item.attributes.validationRules.Length  >0)
+                {
+                    input.Append($"type=\"{item.attributes.validationRules[0].type}\"");
+                }
+                else
+                {
+                   input.Append($"type=\"{item.type}\"");
+                }
+
+                if(!string.IsNullOrEmpty(item.attributes.message))
+                {
+                    input.Append($"messsage=\"{item.attributes.message}\""); 
+                }
+                if(!string.IsNullOrEmpty(item.attributes.name))
+                {
+                    input.Append($"name=\"{item.attributes.name}\""); 
+                }
+                if(!string.IsNullOrEmpty(item.attributes.placeholder))
+                {
+                    input.Append($"placeholder=\"{item.attributes.placeholder}\""); 
+                }
+                if(item.attributes.required)
+                {
+                    input.Append($"required"); 
+                }
+                if(!string.IsNullOrEmpty(item.attributes.value))
+                {
+                    input.Append($"value=\"{item.attributes.value}\""); 
+                }
+                input.Append("/>");
+                if(!string.IsNullOrEmpty(item.attributes.ClassName))
+                {
+                    input.Append($"class=\"{item.attributes.ClassName}\""); 
+                }
+                if(!string.IsNullOrEmpty(item.attributes.value))
+                {
+                    input.Append($"value=\"{item.attributes.value}\""); 
+                }
+                if(item.attributes.disabled)
+                {
+                    input.Append($"disabled"); 
+                }
+                if(item.attributes.selected)
+                {
+                    input.Append($"selected"); 
+                }
+                if(item.attributes.check)
+                {
+                    input.Append($"check"); 
+                }
+                input.Append("/>");
+
+                input.Append("/>");
+                result.Append(input.ToString());
+
+            }
+            result.Append("</form></body></html>");
+            string writePath = @"..\htmlText.html";
+            using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+            {
+                sw.Write(result.ToString());
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
